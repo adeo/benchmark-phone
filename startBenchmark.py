1adba869e1fe1c3b3a43c328ddabe078fda95f98
10130449 <matthieu.bouamama@leroymerlin.fr>
2020-01-03 16:37:15 +0100

    sending analytics

--- startBenchmark.py
+++ startBenchmark.py
@@ -3,11 +3,34 @@
 import glob
 import os.path
 from xml.etree import ElementTree
-os.chdir('benchmark/build\outputs/androidTest-results\connected')
-files = []
-for file in glob.glob("*.xml"):
-    files.append(file)
+import requests
+
+
+def sendEvent(file):
+    params = dict()
+    params['token_auth'] = token
     tree = ElementTree.parse(file)
-    d = dict()
+    params["rec"] = "1"
+    params["idsite"] = "1"
+    params["dimension1"] = tree.find(".//property[@name='device']").get("value")
+    params["e_c"] = "session"
+    params["e_a"] = "benchmark"
     for testcase in tree.findall("testcase"):
-        d[testcase.get('name')] = testcase.get('time')
+        params["e_n"] = testcase.get('name')
+        params["e_v"] = testcase.get('time')
+        print(params)
+        requests.get(base_url, params=params)
+
+
+def reportBenchmark():
+    os.chdir('benchmark/build/outputs/androidTest-results/connected')
+    for file in glob.glob("*.xml"):
+        sendEvent(file)
+
+        
+
+
+base_url = "http://benchmark-analytics-open.apps.op.acp.adeo.com/matomo.php"
+token = "0b44547f5f0c9d526b46247c65aeef57"
+reportBenchmark()
+
