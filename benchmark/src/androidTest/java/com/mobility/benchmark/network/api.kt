0bf393d99aee6022eda120c0e83b10fcae9add9d
10130449 <matthieu.bouamama@leroymerlin.fr>
2020-01-03 10:36:02 +0100

    disable cache

--- benchmark/src/androidTest/java/com/mobility/benchmark/network/api.kt
+++ benchmark/src/androidTest/java/com/mobility/benchmark/network/api.kt
@@ -5,9 +5,10 @@ import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Streaming
-import retrofit2.http.Url
 
 interface api {
+
+    @Headers("Cache-control: no-cache")
     @Streaming
     @GET("/files/1Gb.dat")
     fun downloadFile(): Observable<Response<ResponseBody>>
